7f1878068158f3e7382a42bd1cefb23334a7db14
My name <me@example.com>
2021-04-01 00:36:43 +0530

    third commit

--- work/work/settings.py
+++ work/work/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'work.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3-2',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
